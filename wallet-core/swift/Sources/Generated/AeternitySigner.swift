d05f326f5715887db475e6287a8ba1fb73df7993
ackratos <zhaocong89@gmail.com>
2020-12-08 15:05:22 +0800

    support eddsa

--- wallet-core/swift/Sources/Generated/AeternitySigner.swift
+++ wallet-core/swift/Sources/Generated/AeternitySigner.swift
@@ -20,6 +20,26 @@ public final class AeternitySigner {
         return try! TW_Aeternity_Proto_SigningOutput(serializedData: resultData)
     }
 
+    public static func message(data: TW_Aeternity_Proto_SigningInput) -> Data {
+        let dataData = TWDataCreateWithNSData(try! data.serializedData())
+        defer {
+            TWDataDelete(dataData)
+        }
+        return TWDataNSData(TWAeternitySignerMessage(dataData))
+    }
+
+    public static func transaction(data: TW_Aeternity_Proto_SigningInput, signature: Data) -> String {
+        let dataData = TWDataCreateWithNSData(try! data.serializedData())
+        defer {
+            TWDataDelete(dataData)
+        }
+        let signatureData = TWDataCreateWithNSData(signature)
+        defer {
+            TWDataDelete(signatureData)
+        }
+        return TWStringNSString(TWAeternitySignerTransaction(dataData, signatureData))
+    }
+
     let rawValue: OpaquePointer
 
     init(rawValue: OpaquePointer) {
